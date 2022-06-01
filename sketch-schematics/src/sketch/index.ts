d1a961cecdc318d5a2aa1993d9d98b6e80830459
Ted Jandro <tjandro93@gmail.com>
2022-05-31 20:34:12 -0400

    Continue working on schematics

--- sketch-schematics/src/sketch/index.ts
+++ sketch-schematics/src/sketch/index.ts
@@ -13,8 +13,6 @@ import {
 } from '@angular-devkit/schematics';
 import { Schema } from './schema';
 
-// You don't have to export the function as default. You can also have more than one rule factory
-// per file.
 export function sketch(options: Schema): Rule {
   return async (_tree: Tree, _context: SchematicContext) => {
     if (!options.name) {
--- sketch-schematics/src/sketch/schema.ts
+++ sketch-schematics/src/sketch/schema.ts
@@ -1,10 +1,4 @@
 export interface Schema {
   // The name of the sketch.
   name: string;
-
-  // The path to create the sketch.
-  path?: string;
-
-  // The name of the project.
-  project?: string;
 }
